b14d4a438b26afcf3b1cb643dfe4e4582519f3da
yunyun827 <138848565+yunyun827@users.noreply.github.com>
2024-09-03 18:33:51 +0900

    Title: 정수 명령 처리 8, Time: 24ms, Memory: 0MB, Status: Passed - CodeTree

--- 240903/정수 명령 처리 8/process-numeric-commands-8.cpp
+++ 240903/정수 명령 처리 8/process-numeric-commands-8.cpp
@@ -1,39 +1,55 @@
 #include <iostream>
 #include <list>
 using namespace std;
+
 list<int> li;
 int n, A;
 string s;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     cin >> n;
-   while (n>0){
-     
-     if(s=="push_front"){
-        cin >> A;
-        li.push_front(A);
-     }else if(s=="push_back"){
-        cin >> A;
-        li.push_back(A);
-     }else if (s=="pop_front"){
-        cout << li.head << "\n";
-        li.pop_front();
-     }else if (s=="pop_back"){
-        cout << li.tail << "\n";
-        li.pop_back();
-     }else if (s=="size"){
-        cout << li.size << "\n";
-     }else if (s=="empty"){
-        if(li.head==null && li.tail==null){
-            cout << 0;
-        }else cout << 1;
-     }else if (s=="front"){
-        cout << li.front() << "\n";
-     }else if (s=="back"){
-        cout << li.back() << "\n";
-     }
-   }
+    while (n > 0) {
+        cin >> s; // 명령어 입력 받기
+
+        if (s == "push_front") {
+            cin >> A;
+            li.push_front(A);
+        } else if (s == "push_back") {
+            cin >> A;
+            li.push_back(A);
+        } else if (s == "pop_front") {
+            if (!li.empty()) {
+                cout << li.front() << "\n";
+                li.pop_front();
+            } else {
+                cout << -1 << "\n"; // 리스트가 비어 있을 경우 -1 출력
+            }
+        } else if (s == "pop_back") {
+            if (!li.empty()) {
+                cout << li.back() << "\n";
+                li.pop_back();
+            } else {
+                cout << -1 << "\n"; // 리스트가 비어 있을 경우 -1 출력
+            }
+        } else if (s == "size") {
+            cout << li.size() << "\n";
+        } else if (s == "empty") {
+            cout << (li.empty() ? 1 : 0) << "\n";
+        } else if (s == "front") {
+            if (!li.empty()) {
+                cout << li.front() << "\n";
+            } else {
+                cout << -1 << "\n"; // 리스트가 비어 있을 경우 -1 출력
+            }
+        } else if (s == "back") {
+            if (!li.empty()) {
+                cout << li.back() << "\n";
+            } else {
+                cout << -1 << "\n"; // 리스트가 비어 있을 경우 -1 출력
+            }
+        }
+        n--; // 명령어를 처리했으므로 n을 감소시킴
+    }
 
     return 0;
 }
